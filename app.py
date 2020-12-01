cfe5eb9a309f6152a3a975a229ac9e8c3ae98e34
Veronika Kylevskaya <mrishkaa@gmail.com>
2020-12-01 09:00:10 +0000

    added jsonify

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f'Hello!'
+    return jsonify(message='Hello Flask app from Docker container')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
